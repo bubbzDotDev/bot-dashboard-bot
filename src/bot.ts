279d5e2b4f3434024794a4594b7f21fd2bc94397
bubbzDotDev <christopher.blount@outlook.com>
2022-03-22 10:50:29 -0500

    Connected to DB

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,10 @@
 import { config } from 'dotenv';
 config();
+import 'reflect-metadata';
 import { registerCommands, registerEvents } from './utils/registry';
 import DiscordClient from './client/client';
 import { Collection, Intents } from 'discord.js';
+import { DataSource } from 'typeorm';
 
 const client = new DiscordClient({
   intents: [
@@ -12,10 +14,25 @@ const client = new DiscordClient({
   ],
 });
 
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_DB_HOST,
+  port: +process.env.MYSQL_DB_PORT, // + converts the string to a number
+  username: process.env.MYSQL_DB_USERNAME,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB_DATABASE,
+  synchronize: true,
+});
+
 (async () => {
   client.prefix = process.env.DISCORD_BOT_PREFIX || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.DISCORD_BOT_TOKEN);
+  await AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error));
 })();
 
