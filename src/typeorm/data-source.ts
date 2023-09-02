10e9580f07dbfbcc52b527f6ced9a73d43193c91
Christopher Blount <bubbz@bubbz.dev>
2023-09-02 07:10:17 -0600

    Progress updating app

--- src/typeorm/data-source.ts
+++ src/typeorm/data-source.ts
@@ -10,4 +10,7 @@ export const AppDataSource = new DataSource({
   database: process.env.MYSQL_DB_DATABASE,
   synchronize: JSON.parse(process.env.MYSQL_DB_SYNCHRONIZE), // true while in development, false in production
   entities: [ GuildConfiguration ],
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
